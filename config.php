d7b9279b6062360e4a010604d96ad55abd29aee6
JEMIMA FONSECA PASSOS <48484730+jemima1992@users.noreply.github.com>
2021-01-05 09:12:38 -0300

    delivery

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php 
+
+$email_adm = 'hvfadvocacia@gmail.com';
+$url_site = 'http://localhost/delivery/';
+
+
+//DADOS PARA CONEXÃO COM BD LOCAL
+$banco = 'delivery';
+$host = 'localhost';
+$usuario = 'root';
+$senha = '';
+
+
+//CONFIGURAÇÕES PARA PAGINAÇÃO DE ITENS NO PAINEL
+//Valor padrão para as paginações
+$itens_por_pagina = 5;
+
+//valor que o usuario pode alterar para mostrar a paginação
+$itens_por_pagina_1 = 5;
+$itens_por_pagina_2 = 10;
+$itens_por_pagina_3 = 20;
+ ?>
